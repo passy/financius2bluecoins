e00d7666d66f8ddcb1c00ebd9c6ec0a5da8c6d5c
Pascal Hartig <phartig@rdrei.net>
2017-04-28 17:46:14 +0200

    Read from all sources, parse some JSON

--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,64 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE DeriveGeneric #-}
 
 module Main where
 
 import Protolude
+import Data.Aeson.Lens
+import Control.Lens
 import Options.Generic (ParseRecord, getRecord)
+import Data.Aeson ((.:), (.:?))
+
+import Data.String (String)
+
+import qualified Data.Aeson as Aeson
+import qualified Control.Monad.Logger as L
+import qualified Data.Vector as V
+import qualified Database.SQLite.Simple as SQL
+import qualified Data.HashMap.Strict as HMS
+import qualified Data.Text.Encoding as TE
+import qualified Data.ByteString.Lazy as BSL
+import qualified Data.Text as T
 
 data Args = Args
   { financiusFile :: FilePath
   , bluecoinFile :: FilePath
+  , mappingFile :: FilePath
   } deriving (Eq, Show, Generic, ParseRecord)
 
+type AccountMapping = HMS.HashMap Text Text
+
+data FinanciusTransaction = FinanciusTransaction
+  { ftxAccountFromId :: Maybe Text
+  , ftxAccountToId :: Maybe Text
+  , ftxNote :: Text
+  } deriving (Eq, Show, Generic)
+
+instance Aeson.FromJSON FinanciusTransaction where
+  parseJSON = Aeson.withObject "transaction" $ \o ->
+    FinanciusTransaction
+        <$> o .:? "account_from_id"
+        <*> o .:? "account_to_id"
+        <*> o .: "note"
+
 main :: IO ()
-main = do
+main = L.runStderrLoggingT $ do
+  $(L.logInfo) "Getting started ..."
   args :: Args <- getRecord "financius2bluecoin"
-  print args
+  financiusJson <- liftIO . readFile $ financiusFile args
+  mappingJson <- liftIO . readFile $ mappingFile args
+  conn <- liftIO . S.open $ bluecoinFile args
+
+  let mapping :: Either Text (HMS.HashMap Text Text) =
+        case first T.pack . Aeson.eitherDecodeStrict $ TE.encodeUtf8 mappingJson of
+          Left e -> error $ "mapping parsing failed: " <> e
+          Right a -> a
+
+  let transactions = fromMaybe V.empty $ financiusJson ^? key "transactions" . _Array
+  forM_ transactions $ \tx -> do
+    print $ ftxNote <$> Aeson.fromJSON tx
+
+  $(L.logInfo) "Done."
