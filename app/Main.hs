daae44a26b7ed02ea6720b061d9bcd5204f7edce
Pascal Hartig <phartig@rdrei.net>
2017-04-30 15:53:15 +0200

    Testing writes to the DB

--- app/Main.hs
+++ app/Main.hs
@@ -12,7 +12,6 @@ import Control.Lens
 import Options.Generic (ParseRecord, getRecord)
 import Data.Aeson ((.:), (.:?))
 
-import Data.String (String)
 
 import qualified Data.Aeson as Aeson
 import qualified Control.Monad.Logger as L
@@ -20,7 +19,6 @@ import qualified Data.Vector as V
 import qualified Database.SQLite.Simple as SQL
 import qualified Data.HashMap.Strict as HMS
 import qualified Data.Text.Encoding as TE
-import qualified Data.ByteString.Lazy as BSL
 import qualified Data.Text as T
 
 data Args = Args
@@ -30,12 +28,6 @@ data Args = Args
   } deriving (Eq, Show, Generic, ParseRecord)
 
 type AccountMapping = HMS.HashMap Text Text
-
-data FinanciusTransaction = FinanciusTransaction
-  { ftxAccountFromId :: Maybe Text
-  , ftxAccountToId :: Maybe Text
-  , ftxNote :: Text
-  } deriving (Eq, Show, Generic)
 
 data BluecoinTransaction = BluecoinTransaction
   { btxItemId :: Integer
@@ -49,6 +41,15 @@ instance Aeson.FromJSON FinanciusTransaction where
         <$> o .:? "account_from_id"
         <*> o .:? "account_to_id"
         <*> o .: "note"
+        <*> o .: "amount"
+
+data FinanciusTransaction = FinanciusTransaction
+  { ftxAccountFromId :: Maybe Text
+  , ftxAccountToId :: Maybe Text
+  , ftxNote :: Text
+  , ftxAmount :: Integer
+  } deriving (Eq, Show, Generic)
+
 
 main :: IO ()
 main = L.runStderrLoggingT $ do
@@ -56,15 +57,21 @@ main = L.runStderrLoggingT $ do
   args :: Args <- getRecord "financius2bluecoin"
   financiusJson <- liftIO . readFile $ financiusFile args
   mappingJson <- liftIO . readFile $ mappingFile args
-  conn <- liftIO . S.open $ bluecoinFile args
+  conn <- liftIO . SQL.open $ bluecoinFile args
 
-  let mapping :: Either Text (HMS.HashMap Text Text) =
+  let accountsMap :: Either Text (HMS.HashMap Text Text) =
         case first T.pack . Aeson.eitherDecodeStrict $ TE.encodeUtf8 mappingJson of
           Left e -> error $ "mapping parsing failed: " <> e
           Right a -> a
 
   let transactions = fromMaybe V.empty $ financiusJson ^? key "transactions" . _Array
   forM_ transactions $ \tx -> do
-    print $ ftxNote <$> Aeson.fromJSON tx
+    case ftxNote <$> Aeson.fromJSON tx of
+      Aeson.Success v ->
+        liftIO $ SQL.execute conn "INSERT INTO ITEMTABLE (itemName) VALUES (?)" (SQL.Only v)
+      Aeson.Error e ->
+        $(L.logError) $ "Couldn't parse record: " <> show e
+
+    -- executeNamed "INSERT INTO TRANSACTIONTABLE (amount, itemID, transactionCurrency, conversionRateNew, transactionTypeID, categoryID, accountID, )"
 
   $(L.logInfo) "Done."
