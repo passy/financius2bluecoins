1488cae01d5a8cae9ece5f6d18927debf51a21d2
Pascal Hartig <phartig@rdrei.net>
2017-09-02 15:32:14 +0100

    use new financius file

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 
 cp bluecoins_empty.fydb bluecoins_workingcopy.fydb && \
 stack build --fast && \
-stack exec financius2bluecoin -- --financiusFile Financius\ 2017-08-23\ 184323.json --bluecoinFile bluecoins_workingcopy.fydb && \
+stack exec financius2bluecoin -- --financiusFile Financius\ 2017-09-02\ 152937.json --bluecoinFile bluecoins_workingcopy.fydb && \
 adb push bluecoins_workingcopy.fydb /sdcard/Bluecoins/Database_Export/bluecoins_bak.fydb
 
 # vim:tw=0
