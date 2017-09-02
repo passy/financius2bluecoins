c28b8c29ae0e5c1b8010f23c5b4f88dc0dd6bdd0
Pascal Hartig <phartig@rdrei.net>
2017-09-02 12:55:25 +0100

    Use brittany for source formatting

--- app/Main.hs
+++ app/Main.hs
@@ -178,80 +178,101 @@ getOrCreate
 getOrCreate conn getFn createFn = do
   res <- liftIO $ getFn conn
   case head res of
-    Just a -> return a
+    Just a  -> return a
     Nothing -> liftIO $ do
       createFn conn
       RowId <$> SQL.lastInsertRowId conn
 
 getOrCreateItem :: MonadIO io => SQL.Connection -> Text -> io RowId
 getOrCreateItem conn name = do
-  getOrCreate conn
-    (\c -> SQL.query c "SELECT itemTableId FROM ITEMTABLE WHERE itemName = ?" (SQL.Only name))
-    (\c -> SQL.execute c "INSERT INTO ITEMTABLE (itemName) VALUES (?)" (SQL.Only name))
+  getOrCreate
+    conn
+    ( \c -> SQL.query c
+                      "SELECT itemTableId FROM ITEMTABLE WHERE itemName = ?"
+                      (SQL.Only name)
+    )
+    ( \c -> SQL.execute c
+                        "INSERT INTO ITEMTABLE (itemName) VALUES (?)"
+                        (SQL.Only name)
+    )
 
-toLookupMap ::
-  (Hashable.Hashable b, Eq b) =>
-  (a -> (b, a)) ->
-  V.Vector a ->
-  HMS.HashMap b a
-toLookupMap extract =
-  HMS.fromList . V.toList . V.map extract
+toLookupMap
+  :: (Hashable.Hashable b, Eq b)
+  => (a -> (b, a))
+  -> V.Vector a
+  -> HMS.HashMap b a
+toLookupMap extract = HMS.fromList . V.toList . V.map extract
 
-toFinanciusCategoryLookupMap :: V.Vector FinanciusCategory -> HMS.HashMap Text FinanciusCategory
+toFinanciusCategoryLookupMap
+  :: V.Vector FinanciusCategory -> HMS.HashMap Text FinanciusCategory
 toFinanciusCategoryLookupMap =
-  toLookupMap (\cat@FinanciusCategory{..} -> (fcatName, cat))
+  toLookupMap (\cat@FinanciusCategory {..} -> (fcatName, cat))
 
-toFinanciusAccountLookupMap :: V.Vector FinanciusAccount -> HMS.HashMap Text FinanciusAccount
+toFinanciusAccountLookupMap
+  :: V.Vector FinanciusAccount -> HMS.HashMap Text FinanciusAccount
 toFinanciusAccountLookupMap =
-  toLookupMap (\acc@FinanciusAccount{..} -> (faccName, acc))
+  toLookupMap (\acc@FinanciusAccount {..} -> (faccName, acc))
 
-toFinanciusTagLookupMap :: V.Vector FinanciusTag -> HMS.HashMap Text FinanciusTag
-toFinanciusTagLookupMap =
-  toLookupMap (\tag@FinanciusTag{..} -> (ftagId, tag))
+toFinanciusTagLookupMap
+  :: V.Vector FinanciusTag -> HMS.HashMap Text FinanciusTag
+toFinanciusTagLookupMap = toLookupMap (\tag@FinanciusTag {..} -> (ftagId, tag))
 
 vecCatMaybes :: V.Vector (Maybe a) -> V.Vector a
 vecCatMaybes = V.concatMap f
-  where
-        f :: forall a. Maybe a -> V.Vector a
-        f (Just a) = V.singleton a
-        f Nothing = V.empty
+ where
+  f :: forall a . Maybe a -> V.Vector a
+  f (Just a) = V.singleton a
+  f Nothing  = V.empty
 
 decodeJSONArray
-  :: (AsValue s, Aeson.FromJSON a)
-  => Text
-  -> s
-  -> Maybe (V.Vector a)
-decodeJSONArray key_ json =
-  sequenceA $ join <$> sequenceA (fmap (hush . decodeValueEither) <$> (json ^? key key_. _Array))
+  :: (AsValue s, Aeson.FromJSON a) => Text -> s -> Maybe (V.Vector a)
+decodeJSONArray key_ json = sequenceA $ join <$> sequenceA
+  (fmap (hush . decodeValueEither) <$> (json ^? key key_ . _Array))
 
 main :: IO ()
 main = L.runStderrLoggingT $ do
   $(L.logInfo) "Getting started ..."
-  args :: Args <- getRecord "financius2bluecoin"
+  args :: Args  <- getRecord "financius2bluecoin"
   financiusJson <- liftIO . readFile $ financiusFile args
-  conn <- liftIO . SQL.open $ bluecoinFile args
+  conn          <- liftIO . SQL.open $ bluecoinFile args
 
 
   -- I'm sure there's a better way for this. I must be ignoring some useful law here.
-  let fAccounts :: HMS.HashMap Text FinanciusAccount =
-        maybe (error "parsing accounts failed") toFinanciusAccountLookupMap (decodeJSONArray "accounts" financiusJson)
+  let fAccounts :: HMS.HashMap Text FinanciusAccount = maybe
+        (error "parsing accounts failed")
+        toFinanciusAccountLookupMap
+        (decodeJSONArray "accounts" financiusJson)
   bAccounts <- getBluecoinAccounts conn
-  mergedAccounts :: HMS.HashMap Text BluecoinAccount <- mergeAccounts bAccounts fAccounts
+  mergedAccounts :: HMS.HashMap Text BluecoinAccount <- mergeAccounts
+    bAccounts
+    fAccounts
 
-  let fCategories :: HMS.HashMap Text FinanciusCategory =
-        maybe (error "parsing accounts failed") toFinanciusCategoryLookupMap (decodeJSONArray "categories" financiusJson)
+  let fCategories :: HMS.HashMap Text FinanciusCategory = maybe
+        (error "parsing accounts failed")
+        toFinanciusCategoryLookupMap
+        (decodeJSONArray "categories" financiusJson)
   bCategories <- getBluecoinCategories conn
-  mergedCategories :: HMS.HashMap Text BluecoinCategory <- mergeCategories bCategories fCategories
+  mergedCategories :: HMS.HashMap Text BluecoinCategory <- mergeCategories
+    bCategories
+    fCategories
 
-  let fTags :: HMS.HashMap Text FinanciusTag =
-        maybe (error "parsing tags failed") (toFinanciusTagLookupMap) (decodeJSONArray "tags" financiusJson)
+  let fTags :: HMS.HashMap Text FinanciusTag = maybe
+        (error "parsing tags failed")
+        (toFinanciusTagLookupMap)
+        (decodeJSONArray "tags" financiusJson)
 
-  let transactions = fromMaybe V.empty $ financiusJson ^? key "transactions" . _Array
+  let transactions =
+        fromMaybe V.empty $ financiusJson ^? key "transactions" . _Array
 
-  maybeBtxs :: V.Vector (Maybe BluecoinTransaction) <- forM transactions $ \tx -> do
+  maybeBtxs :: V.Vector (Maybe BluecoinTransaction) <-
+    forM transactions $ \tx -> do
       case Aeson.fromJSON tx of
-        Aeson.Success ftx -> runMaybeT $
-          mkBluecoinTransaction conn mergedAccounts mergedCategories fTags ftx
+        Aeson.Success ftx -> runMaybeT $ mkBluecoinTransaction
+          conn
+          mergedAccounts
+          mergedCategories
+          fTags
+          ftx
         Aeson.Error e -> do
           $(L.logError) $ "Couldn't parse transaction: " <> show e
           return Nothing
@@ -266,20 +287,18 @@ mergeCategories
   => [BluecoinCategory]
   -> HMS.HashMap Text FinanciusCategory
   -> m (HMS.HashMap Text BluecoinCategory)
-mergeCategories =
-  mergeLookupMaps bcatName fcatId
+mergeCategories = mergeLookupMaps bcatName fcatId
 
 mergeAccounts
   :: L.MonadLogger m
   => [BluecoinAccount]
   -> HMS.HashMap Text FinanciusAccount
   -> m (HMS.HashMap Text BluecoinAccount)
-mergeAccounts =
-  mergeLookupMaps baccName faccId
+mergeAccounts = mergeLookupMaps baccName faccId
 
 mergeLookupMaps
-  :: forall a b (m :: * -> *).
-    (Eq a, Eq b, L.MonadLogger m)
+  :: forall a b (m :: * -> *)
+   . (Eq a, Eq b, L.MonadLogger m)
   => (b -> Text)
   -> (a -> Text)
   -> [b]
@@ -288,58 +307,57 @@ mergeLookupMaps
 mergeLookupMaps extractName extractKey list' lookupMap = do
   vals <- catMaybes <$> mapM extract list'
   return $ HMS.fromList vals
-  where
-    extract
-      :: b
-      -> m (Maybe (Text, b))
-    extract element' =
-      case HMS.lookup (extractName element') lookupMap of
-        Just res -> return $ Just (extractKey res, element')
-        Nothing -> do
-          $(L.logError) $ "Could not find corresponding lookup element '" <> extractName element' <> "'."
-          return Nothing
+ where
+  extract :: b -> m (Maybe (Text, b))
+  extract element' = case HMS.lookup (extractName element') lookupMap of
+    Just res -> return $ Just (extractKey res, element')
+    Nothing  -> do
+      $(L.logError)
+        $  "Could not find corresponding lookup element '"
+        <> extractName element'
+        <> "'."
+      return Nothing
 
-getBluecoinCategories
-  :: MonadIO io
-  => SQL.Connection
-  -> io [BluecoinCategory]
-getBluecoinCategories conn =
-  liftIO $ SQL.query_ conn "SELECT categoryTableID, childCategoryName FROM CHILDCATEGORYTABLE"
+getBluecoinCategories :: MonadIO io => SQL.Connection -> io [BluecoinCategory]
+getBluecoinCategories conn = liftIO $ SQL.query_
+  conn
+  "SELECT categoryTableID, childCategoryName FROM CHILDCATEGORYTABLE"
 
-getBluecoinAccounts
-  :: MonadIO io
-  => SQL.Connection
-  -> io [BluecoinAccount]
-getBluecoinAccounts conn =
-  liftIO $ SQL.query_ conn "SELECT accountsTableID, accountName, accountCurrency FROM ACCOUNTSTABLE"
+getBluecoinAccounts :: MonadIO io => SQL.Connection -> io [BluecoinAccount]
+getBluecoinAccounts conn = liftIO $ SQL.query_
+  conn
+  "SELECT accountsTableID, accountName, accountCurrency FROM ACCOUNTSTABLE"
 
 decodeValueEither :: (Aeson.FromJSON a) => Aeson.Value -> Either Text a
 decodeValueEither v = case Aeson.fromJSON v of
   Aeson.Success res -> pure res
-  Aeson.Error e -> Left $ T.pack e
+  Aeson.Error   e   -> Left $ T.pack e
 
 getBtxAccount
   :: L.MonadLogger m
   => HMS.HashMap Text BluecoinAccount
   -> FinanciusTransaction
   -> MaybeT m TransactionBundle
-getBtxAccount baccs FinanciusTransaction{..} = do
+getBtxAccount baccs FinanciusTransaction {..} = do
   -- My Lord, this turned out really nicely.
   case ftxTransactionType of
-    Expense -> Single <$> lookup ftxAccountFromId
-    Income -> Single <$> lookup ftxAccountToId
+    Expense  -> Single <$> lookup ftxAccountFromId
+    Income   -> Single <$> lookup ftxAccountToId
     Transfer -> Double <$> lookup ftxAccountFromId <*> lookup ftxAccountToId
-
-  where
-    lookup
-      :: L.MonadLogger m
-      => Maybe Text
-      -> MaybeT m BluecoinAccount
-    lookup field =
-      do MaybeT $ case (flip HMS.lookup) baccs =<< field of
-          Nothing | isJust ftxAccountFromId -> ($(L.logError) $ "Invariant violation: account not in mapping list: " <> show field) >> pure Nothing
-                  | otherwise -> pure Nothing
-          Just btxAccount -> pure $ Just btxAccount
+ where
+  lookup :: L.MonadLogger m => Maybe Text -> MaybeT m BluecoinAccount
+  lookup field = do
+    MaybeT $ case (flip HMS.lookup) baccs =<< field of
+      Nothing
+        | isJust ftxAccountFromId
+        -> (  $(L.logError)
+           $  "Invariant violation: account not in mapping list: "
+           <> show field
+           )
+          >> pure Nothing
+        | otherwise
+        -> pure Nothing
+      Just btxAccount -> pure $ Just btxAccount
 
 mkBluecoinTransaction
   :: (MonadIO io, L.MonadLogger io)
@@ -349,122 +367,120 @@ mkBluecoinTransaction
   -> HMS.HashMap Text FinanciusTag
   -> FinanciusTransaction
   -> MaybeT io BluecoinTransaction
-mkBluecoinTransaction conn baccs bcats ftags ftx@FinanciusTransaction{..} = do
-  let itemName =
-        if T.null ftxNote
-          then "(Unnamed transaction)"
-          else ftxNote
+mkBluecoinTransaction conn baccs bcats ftags ftx@FinanciusTransaction {..} = do
+  let itemName = if T.null ftxNote then "(Unnamed transaction)" else ftxNote
   btxItemId :: RowId <- getOrCreateItem conn itemName
-  let btxNotes = "passyImportId:" <> ftxId
+  let btxNotes  = "passyImportId:" <> ftxId
   let btxAmount = ftxAmount * 10000
-  let btxDate = PClock.posixSecondsToUTCTime $ realToFrac $ ftxDate `div` 1000
-  let btxLabels :: [Text] = ftagName <$> catMaybes (flip HMS.lookup ftags <$> ftxTagIds)
-  let btxConversionRate = ftxExchangeRate
+  let btxDate   = PClock.posixSecondsToUTCTime $ realToFrac $ ftxDate `div` 1000
+  let btxLabels :: [Text] =
+        ftagName <$> catMaybes (flip HMS.lookup ftags <$> ftxTagIds)
+  let btxConversionRate  = ftxExchangeRate
   let btxTransactionType = ftxTransactionType
 
-  btxAccount <- getBtxAccount baccs ftx
+  btxAccount    <- getBtxAccount baccs ftx
 
   -- FIXME: This doesn't actually fall back.
-  btxCategoryId <- MaybeT $ case (flip HMS.lookup) bcats (fromMaybe transferCategoryName ftxCategoryId) of
-    Nothing -> ($(L.logError) $ "Invariant violation: category id not populated: " <> show ftxCategoryId) >> pure Nothing
-    Just BluecoinCategory{..} -> pure $ Just bcatId
+  btxCategoryId <-
+    MaybeT
+      $ case
+          (flip HMS.lookup) bcats (fromMaybe transferCategoryName ftxCategoryId)
+        of
+          Nothing ->
+            (  $(L.logError)
+              $  "Invariant violation: category id not populated: "
+              <> show ftxCategoryId
+              )
+              >> pure Nothing
+          Just BluecoinCategory {..} -> pure $ Just bcatId
 
-  return $ BluecoinTransaction{..}
+  return $ BluecoinTransaction {..}
 
 mkBluecoinAccount :: AccountMapping -> FinanciusAccount -> Maybe BluecoinAccount
-mkBluecoinAccount accountMapping FinanciusAccount{..} = do
+mkBluecoinAccount accountMapping FinanciusAccount {..} = do
   let baccCurrencyCode = faccCurrencyCode
-  let baccName = faccName
+  let baccName         = faccName
   baccId <- RowId <$> HMS.lookup faccId accountMapping
-  pure BluecoinAccount{..}
+  pure BluecoinAccount {..}
 
 writeBluecoinTransaction
   :: forall (io :: * -> *)
-  .  (MonadIO io, L.MonadLogger io)
+   . (MonadIO io, L.MonadLogger io)
   => SQL.Connection
   -> BluecoinTransaction
   -> io ()
-writeBluecoinTransaction conn btx@BluecoinTransaction{..} = do
+writeBluecoinTransaction conn btx@BluecoinTransaction {..} = do
   let amount = case btxTransactionType of
-        Expense -> negate btxAmount
-        Income -> btxAmount
+        Expense  -> negate btxAmount
+        Income   -> btxAmount
         Transfer -> 0
 
   txIds <- case btxAccount of
-        Single account -> do
-          txId <- write amount account account
-          setTxPairId conn txId txId
-          return [txId]
-        Double srcAccount destAccount ->
-          -- I know this is a very lazy encoding of this invariant ... Maybe MonadPlus at least?
-          if btxTransactionType /= Transfer then
-            ($(L.logError) $ "Invalid Double transaction with non-transfer type: " <> show btx) >> return []
-          else do
-            srcTxId <- write (negate btxAmount) srcAccount destAccount
-            destTxId <- write btxAmount destAccount srcAccount
-            return [srcTxId, destTxId]
+    Single account -> do
+      txId <- write amount account account
+      setTxPairId conn txId txId
+      return [txId]
+    Double srcAccount destAccount ->
+      -- I know this is a very lazy encoding of this invariant ... Maybe MonadPlus at least?
+                                     if btxTransactionType /= Transfer
+      then
+        (  $(L.logError)
+          $  "Invalid Double transaction with non-transfer type: "
+          <> show btx
+          )
+          >> return []
+      else do
+        srcTxId  <- write (negate btxAmount) srcAccount destAccount
+        destTxId <- write btxAmount destAccount srcAccount
+        return [srcTxId, destTxId]
 
-  forM_ btxLabels $ \label ->
-    forM_ txIds $ \tx ->
-       writeBluecoinLabel conn tx label
-
-  where
-    write
-      :: Integer
-      -> BluecoinAccount
-      -> BluecoinAccount
-      -> io RowId
-    write amount srcAccount destAccount = do
-      let sql :: SQL.Query =
-            "INSERT INTO TRANSACTIONSTABLE (itemID, amount, notes, accountID, transactionCurrency, conversionRateNew, transactionTypeID, categoryID, tags, accountReference, accountPairID, uidPairID, deletedTransaction, hasPhoto, labelCount, date)\
+  forM_ btxLabels
+    $ \label -> forM_ txIds $ \tx -> writeBluecoinLabel conn tx label
+ where
+  write :: Integer -> BluecoinAccount -> BluecoinAccount -> io RowId
+  write amount srcAccount destAccount = do
+    let
+      sql :: SQL.Query
+        = "INSERT INTO TRANSACTIONSTABLE (itemID, amount, notes, accountID, transactionCurrency, conversionRateNew, transactionTypeID, categoryID, tags, accountReference, accountPairID, uidPairID, deletedTransaction, hasPhoto, labelCount, date)\
             \ VALUES\
             \ (:itemID, :amount, :notes, :accountID, :transactionCurrency, :conversionRateNew, :transactionTypeID, :categoryID, :tags, :accountReference, :accountPairID, :uidPairID, :deletedTransaction, :hasPhoto, :labelCount, :date)"
-      liftIO $ SQL.executeNamed conn sql
-        [ ":itemID" := btxItemId
-        , ":amount" := amount
-        , ":notes" := btxNotes
-        , ":date" := btxDate
-        , ":categoryID" := btxCategoryId
-        , ":hasPhoto" := (0 :: Int)
-        , ":labelCount" := length btxLabels
-        , ":conversionRateNew" := btxConversionRate
-        , ":transactionTypeID" := fromEnum btxTransactionType
-        , ":uidPairID" := (-1 :: Int)
-        , ":accountID" := baccId srcAccount
-        , ":accountPairID" := baccId destAccount
-        , ":transactionCurrency" := baccCurrencyCode srcAccount
-        , ":accountReference" := (3 :: Int)
-        , ":deletedTransaction" := (6 :: Int)
-        -- TODO
-        , ":tags" := ("temptags" :: Text)
-        ]
-      liftIO $ RowId <$> SQL.lastInsertRowId conn
+    liftIO $ SQL.executeNamed
+      conn
+      sql
+      [ ":itemID" := btxItemId
+      , ":amount" := amount
+      , ":notes" := btxNotes
+      , ":date" := btxDate
+      , ":categoryID" := btxCategoryId
+      , ":hasPhoto" := (0 :: Int)
+      , ":labelCount" := length btxLabels
+      , ":conversionRateNew" := btxConversionRate
+      , ":transactionTypeID" := fromEnum btxTransactionType
+      , ":uidPairID" := (-1 :: Int)
+      , ":accountID" := baccId srcAccount
+      , ":accountPairID" := baccId destAccount
+      , ":transactionCurrency" := baccCurrencyCode srcAccount
+      , ":accountReference" := (3 :: Int)
+      , ":deletedTransaction" := (6 :: Int)
+      -- TODO
+      , ":tags" := ("temptags" :: Text)
+      ]
+    liftIO $ RowId <$> SQL.lastInsertRowId conn
 
 writeBluecoinLabel
-  :: (MonadIO io, L.MonadLogger io)
-  => SQL.Connection
-  -> RowId
-  -> Text
-  -> io ()
+  :: (MonadIO io, L.MonadLogger io) => SQL.Connection -> RowId -> Text -> io ()
 writeBluecoinLabel conn (RowId rowId) label = do
-  let sql :: SQL.Query =
-        "INSERT INTO LABELSTABLE (labelName, transactionIDLabels)\
+  let
+    sql :: SQL.Query
+      = "INSERT INTO LABELSTABLE (labelName, transactionIDLabels)\
         \ VALUES\
         \ (:label, :rowId)"
-  liftIO $ SQL.executeNamed conn sql
-     [ ":label" := label
-     , ":rowId" := rowId ]
+  liftIO $ SQL.executeNamed conn sql [":label" := label, ":rowId" := rowId]
 
 setTxPairId
-  :: (MonadIO io, L.MonadLogger io)
-  => SQL.Connection
-  -> RowId
-  -> RowId
-  -> io ()
+  :: (MonadIO io, L.MonadLogger io) => SQL.Connection -> RowId -> RowId -> io ()
 setTxPairId conn txId pairId = do
-  let sql :: SQL.Query =
-        "UPDATE TRANSACTIONSTABLE SET uidPairID = :pairId WHERE transactionsTableID = :txId"
-  liftIO $ SQL.executeNamed conn sql
-    [ ":txId" := txId
-    , ":uidPairID" := pairId
-    ]
+  let
+    sql :: SQL.Query
+      = "UPDATE TRANSACTIONSTABLE SET uidPairID = :pairId WHERE transactionsTableID = :txId"
+  liftIO $ SQL.executeNamed conn sql [":txId" := txId, ":uidPairID" := pairId]
