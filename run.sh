ebeba13d037ca272494dcce8208306c12c99b469
Pascal Hartig <phartig@rdrei.net>
2017-09-16 21:24:53 +0000

    Add eurofxref to run command

--- run.sh
+++ run.sh
@@ -4,7 +4,10 @@
 
 cp bluecoins_empty.fydb bluecoins_workingcopy.fydb && \
 stack build --fast && \
-stack exec financius2bluecoin -- --financiusFile Financius\ 2017-09-02\ 152937.json --bluecoinFile bluecoins_workingcopy.fydb && \
+stack exec financius2bluecoin -- \
+      --financiusFile Financius\ 2017-09-02\ 152937.json \
+      --bluecoinFile bluecoins_workingcopy.fydb \
+      --eurofxrefFile eurofxref-hist.zip && \
 adb push bluecoins_workingcopy.fydb /sdcard/Bluecoins/Database_Export/bluecoins_bak.fydb
 
 # vim:tw=0
