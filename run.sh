d8105d09f6b0921f07317dd9408949add93b688d
Pascal Hartig <phartig@rdrei.net>
2017-09-28 23:10:23 +0100

    Update run script + eurofxref

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@
 cp bluecoins_empty.fydb bluecoins_workingcopy.fydb && \
 stack build --fast && \
 stack exec financius2bluecoin -- \
-      --financiusFile Financius\ 2017-09-02\ 152937.json \
+      --financiusFile "Financius 2017-09-28 230129.json" \
       --bluecoinFile bluecoins_workingcopy.fydb \
       --eurofxrefFile eurofxref-hist.zip && \
 adb push bluecoins_workingcopy.fydb /sdcard/Bluecoins/Database_Export/bluecoins_bak.fydb
