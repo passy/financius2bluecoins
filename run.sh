fef6c53b0142794c784ac907517ef9bec436eaf3
Pascal Hartig <phartig@rdrei.net>
2017-09-30 22:28:08 +0100

    I think the maths is sound now?

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@
 cp bluecoins_empty.fydb bluecoins_workingcopy.fydb && \
 stack build --fast && \
 stack exec financius2bluecoin -- \
-      --financiusFile "Financius 2017-09-28 230129.json" \
+      --financiusFile "./Financius 2017-09-30 121507.json" \
       --bluecoinFile bluecoins_workingcopy.fydb \
       --eurofxrefFile eurofxref-hist.zip && \
 adb push bluecoins_workingcopy.fydb /sdcard/Bluecoins/Database_Export/bluecoins_bak.fydb
